d0de146c4f20e7d49785f7fb184b0c3013097b69
Xin <zeeeengxin@users.noreply.github.com>
2017-09-25 16:25:59 -0700

    Add files via upload

--- libft.h
+++ libft.h
@@ -37,5 +37,14 @@ char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+
 
 #endif
