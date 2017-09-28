d523614a75db4a544d92f324eff8a5dad2bac15e
Xin <zeeeengxin@users.noreply.github.com>
2017-09-28 16:47:48 -0700

    Add files via upload

--- libft.h
+++ libft.h
@@ -76,6 +76,7 @@ void				ft_putnbr(int n);
 void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
+void				ft_putnbr_fd(int n, int fd);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
